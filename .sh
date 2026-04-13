507297cd0b96866902b300bffd43b12b02be707e
guoxiuyi123 <2499719702@qq.com>
2026-04-13 16:04:23 +0800

    new

--- .sh
+++ .sh
@@ -13,7 +13,7 @@ CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=2 train.py \
 python train.py -c configs/deimv2/deimv2_dinov3_m_coco.yml --test-only -r outputs/deimv2_dinov3_m_coco/best_stg1.pth
 
 CUDA_VISIBLE_DEVICES=0,1 torchrun --nproc_per_node=2 train.py \
-  -c configs/yaml/deim_dfine_hgnetv2_n_mg.yml \
+  -c /home/pc/gxy/Point-DEIM/configs/yaml/deim_dfine_hgnetv2_n_mg_visdrone_pointonly.yml \
   --use-amp \
   --seed 0 \
   -u train_dataloader.total_batch_size=16
